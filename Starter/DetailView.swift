2eca3f7a59be96312f83ad309fb86de41bb2b440
Asim <saffarrli14@gmail.com>
2024-01-17 17:22:59 +0400

    Initial Commit

---
+++ Starter/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Starter
+//
+//  Created by Asim Seferli on 17.01.24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by Asim Seferli on 17.01.24.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
